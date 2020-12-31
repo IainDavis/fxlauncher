8cf8a950498fb65b545094889becb78c11702683
Iain Davis <iain_davis@intuit.com>
2020-12-30 16:05:48 -0800

    Adding test for and related changes to Resolver.java

--- src/test/java/fxlauncher/config/ResolverTest.java
+++ src/test/java/fxlauncher/config/ResolverTest.java
@@ -1,5 +1,7 @@
 package fxlauncher.config;
 
+import static fxlauncher.model.OS.WIN;
+import static fxlauncher.testutils.ReflectionTools.updateEnv;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.DisplayName;
@@ -13,78 +15,83 @@ import fxlauncher.model.OS;
 
 public class ResolverTest extends ResolverTestHarness {
 
-	@DisplayName("Resolver.DEFAULT...")
-	@ParameterizedTest(name = "returns \"{0}\" unchanged")
-	@ValueSource(strings = { "", "null", "someString" })
-	public void defaultResolverTest(String input) {
-		input = input.equals("null") ? null : input;
-		assertEquals(input, Resolver.DEFAULT.apply(input));
-	}
+  @DisplayName("Resolver.DEFAULT...")
+  @ParameterizedTest(name = "returns \"{0}\" unchanged")
+  @ValueSource(strings = {"", "null", "someString"})
+  public void defaultResolverTest(String input) {
+    input = input.equals("null") ? null : input;
+    assertEquals(input, Resolver.DEFAULT.apply(input));
+  }
 
-	@DisplayName("Resolver.CACHE_DIR...")
-	@Nested
-	class CacheDirResolver {
+  @DisplayName("Resolver.CACHE_DIR...")
+  @Nested
+  class CacheDirResolver {
 
-		@DisplayName("when handling ALLUSERS sentinel...")
-		@Nested
-		class AllUsersTests {
+    @DisplayName("when handling ALLUSERS sentinel...")
+    @Nested
+    class AllUsersTests {
 
-			@DisplayName("resolves leading ALLUSERS for...")
-			@ParameterizedTest(name = "OS {0}")
-			@EnumSource(OS.class)
-			void resolveAllUsersAtStartOfPath(OS os) {
-				setCurrentOS(os);
+      @DisplayName("resolves leading ALLUSERS for...")
+      @ParameterizedTest(name = "OS {0}")
+      @EnumSource(OS.class)
+      void resolveAllUsersAtStartOfPath(OS os) {
+        setCurrentOS(os);
 
-				String inputPath = "ALLUSERS/TEST/PATH";
-				assertEquals(expectedAllUsersPathMap.get(os), Resolver.CACHE_DIR.apply(inputPath));
-			}
+        String inputPath = "ALLUSERS/TEST/PATH";
+        assertEquals(expectedAllUsersPathMap.get(os), Resolver.CACHE_DIR.apply(inputPath));
+      }
 
-			@DisplayName("ignores non-leading ALLUSERS for...")
-			@ParameterizedTest(name = "OS {0}")
-			@EnumSource(OS.class)
-			void ignoreNonLeadingAllUsers(OS os) {
-				setCurrentOS(os);
+      @DisplayName("ignores non-leading ALLUSERS for...")
+      @ParameterizedTest(name = "OS {0}")
+      @EnumSource(OS.class)
+      void ignoreNonLeadingAllUsers(OS os) throws ReflectiveOperationException {
 
-				String inputPath = "TEST/ALLUSERS/PATH";
-				assertEquals(inputPath, Resolver.CACHE_DIR.apply(inputPath));
-			}
-		}
+        if (os == WIN) {
+          updateEnv("ALLUSERSPROFILE", "TEST_ALLUSERS_PROFILE");
+        }
 
-		@DisplayName("when handling USERLIB sentinel...")
-		@Nested
-		class UserLibTests {
+        setCurrentOS(os);
 
-			@DisplayName("resolves leading USERLIB for...")
-			@ParameterizedTest(name = "OS {0}")
-			@EnumSource(OS.class)
-			void resolveAllUsersAtStartOfPath(OS os) {
-				setCurrentOS(os);
+        String inputPath = "TEST/ALLUSERS/PATH";
+        assertEquals(inputPath, Resolver.CACHE_DIR.apply(inputPath));
+      }
+    }
 
-				String inputPath = "USERLIB/TEST/PATH";
-				assertEquals(expectedUserLibPathMap.get(os), Resolver.CACHE_DIR.apply(inputPath));
-			}
+    @DisplayName("when handling USERLIB sentinel...")
+    @Nested
+    class UserLibTests {
 
-			@DisplayName("ignores non-leading ALLUSERS for...")
-			@ParameterizedTest(name = "OS {0}")
-			@EnumSource(OS.class)
-			void ignoreNonLeadingAllUsers(OS os) {
-				setCurrentOS(os);
+      @DisplayName("resolves leading USERLIB for...")
+      @ParameterizedTest(name = "OS {0}")
+      @EnumSource(OS.class)
+      void resolveAllUsersAtStartOfPath(OS os) {
+        setCurrentOS(os);
 
-				String inputPath = "TEST/USERLIB/PATH";
-				assertEquals(inputPath, Resolver.CACHE_DIR.apply(inputPath));
-			}
-		}
+        String inputPath = "USERLIB/TEST/PATH";
+        assertEquals(expectedUserLibPathMap.get(os), Resolver.CACHE_DIR.apply(inputPath));
+      }
 
-		@DisplayName("When handling a null path, returns it unchanged")
-		@Test
-		void testNullHandling() {
-			assertEquals(null, Resolver.CACHE_DIR.apply(null));
-		}
+      @DisplayName("ignores non-leading ALLUSERS for...")
+      @ParameterizedTest(name = "OS {0}")
+      @EnumSource(OS.class)
+      void ignoreNonLeadingAllUsers(OS os) {
+        setCurrentOS(os);
 
-		@DisplayName("When handling an empty String, returns it unchanged")
-		@Test
-		void testEmptyStringHandling() {
-			assertEquals("", Resolver.CACHE_DIR.apply(""));
-		}
-	}
+        String inputPath = "TEST/USERLIB/PATH";
+        assertEquals(inputPath, Resolver.CACHE_DIR.apply(inputPath));
+      }
+    }
+
+    @DisplayName("When handling a null path, returns it unchanged")
+    @Test
+    void testNullHandling() {
+      assertEquals(null, Resolver.CACHE_DIR.apply(null));
+    }
+
+    @DisplayName("When handling an empty String, returns it unchanged")
+    @Test
+    void testEmptyStringHandling() {
+      assertEquals("", Resolver.CACHE_DIR.apply(""));
+    }
+  }
 }
