267a46df6f7554117f77a645bb4d2bbb2efb20e4
Iain Davis <iain_davis@intuit.com>
2020-12-30 16:08:12 -0800

    add test for and related changes to DownstreamParameters.java

--- src/main/java/fxlauncher/downstream/DownstreamParameters.java
+++ src/main/java/fxlauncher/downstream/DownstreamParameters.java
@@ -2,7 +2,7 @@ package fxlauncher.downstream;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -25,98 +25,103 @@ import javafx.application.Application;
  */
 public class DownstreamParameters extends Application.Parameters {
 
-	private static final Pattern NAMED_PATTERN = Pattern.compile("--(?<key>.\\+)=(?<value>.\\+)");
+    public DownstreamParameters() {
+	super();
+    }
 
-	// use Set to simply enforce uniqueness
-	private static Set<String> rawParams = new HashSet<>();
-	private static Set<String> unnamedParams = new HashSet<>();
-	private static Map<String, String> namedParams = new HashMap<>();
+    private static final Pattern NAMED_PATTERN = Pattern.compile("--(?<key>.+)=(?<value>.+)");
 
-	/**
-	 * Return the set of raw arguments
-	 */
-	@Override
-	public List<String> getRaw() {
-		return new ArrayList<>(rawParams);
+    // use Set to simply enforce uniqueness
+    // using LinkedHashSet preserves insertion order
+    private Set<String> rawParams = new LinkedHashSet<>();
+    private Set<String> unnamedParams = new LinkedHashSet<>();
+    private Map<String, String> namedParams = new HashMap<>();
+
+    /**
+     * Return the set of raw arguments
+     */
+    @Override
+    public List<String> getRaw() {
+	return new ArrayList<>(rawParams);
+    }
+
+    /**
+     * Return the set of unnamed arguments (i.e. those not of the format
+     * '--key=value')
+     */
+    @Override
+    public List<String> getUnnamed() {
+	return new ArrayList<>(unnamedParams);
+    }
+
+    /**
+     * Return the set of named arguments (i.e. those of the format '--key=value')
+     */
+    @Override
+    public Map<String, String> getNamed() {
+	return namedParams;
+    }
+
+    /**
+     * Return the set of arguments as a String array This is useful when trying to
+     * launch the downstream program via a {@code main()} method.
+     *
+     * @return the set of arguments as an array
+     */
+    public String[] getArgs() {
+	return rawParams.stream().toArray(String[]::new);
+    }
+
+    /**
+     * Add the argument to the set of downstream parameters, unless it is already
+     * present in the set
+     *
+     * Useful when ingesting a manifest to avoid overwriting any
+     * {@link fxlauncher.config.LauncherConfig} options that were explicitly set by
+     * command-line args, embedded configuration files, or remote overrides
+     *
+     * @param string the argument to be merged into the set of Parameters
+     */
+    public void mergeIfNotPresent(String string) {
+	merge(string, false);
+    }
+
+    /**
+     * Add the argument to the set of downstream parameters, overwriting any
+     * pre-existing value
+     *
+     * Useful when ingesting initial configuration data from command-line args,
+     * embedded configuration files, and remote overrides, where precedence is set
+     * by order and each should override the previous sets of configuration.
+     *
+     * @param string the argument to be merged into the set of Parameters
+     */
+    public void mergeOverwriting(String string) {
+	merge(string, true);
+    }
+
+    /**
+     * Generic merge method that hands both overwriting and non-overwriting cases
+     *
+     * note that overwriting only applies to named parameters uniqueness of
+     * non-named parameters and CLI arguments is ensured by the use of a Set
+     * internally.
+     *
+     * @param string    the argument to be merged into the set of Parameters
+     * @param overwrite a boolean indicating whether existing parameters should be
+     *                  overwritten
+     */
+    public void merge(String string, boolean overwrite) {
+	Matcher matcher = NAMED_PATTERN.matcher(string);
+	if (matcher.find()) {
+	    String key = matcher.group("key");
+	    String value = matcher.group("value");
+	    if (overwrite || !namedParams.containsKey(key)) {
+		namedParams.put(key, value);
+	    }
+	} else {
+	    unnamedParams.add(string);
 	}
-
-	/**
-	 * Return the set of unnamed arguments (i.e. those not of the format
-	 * '--key=value')
-	 */
-	@Override
-	public List<String> getUnnamed() {
-		return new ArrayList<>(unnamedParams);
-	}
-
-	/**
-	 * Return the set of named arguments (i.e. those of the format '--key=value')
-	 */
-	@Override
-	public Map<String, String> getNamed() {
-		return namedParams;
-	}
-
-	/**
-	 * Return the set of arguments as a String array This is useful when trying to
-	 * launch the downstream program via a {@code main()} method.
-	 *
-	 * @return the set of arguments as an array
-	 */
-	public String[] getArgs() {
-		return rawParams.parallelStream().toArray(String[]::new);
-	}
-
-	/**
-	 * Add the argument to the set of downstream parameters, unless it is already
-	 * present in the set
-	 *
-	 * Useful when ingesting a manifest to avoid overwriting any
-	 * {@link LauncherConfig} options that were explicitly set by command-line args,
-	 * embedded configuration files, or remote overrides
-	 *
-	 * @param string the argument to be merged into the set of Parameters
-	 */
-	public void mergeIfNotPresent(String string) {
-		merge(string, false);
-	}
-
-	/**
-	 * Add the argument to the set of downstream parameters, overwriting any
-	 * pre-existing value
-	 *
-	 * Useful when ingesting initial configuration data from command-line args,
-	 * embedded configuration files, and remote overrides, where precedence is set
-	 * by order and each should override the previous sets of configuration.
-	 *
-	 * @param string the argument to be merged into the set of Parameters
-	 */
-	public void mergeOverwriting(String string) {
-		merge(string, true);
-	}
-
-	/**
-	 * Generic merge method that hands both overwriting and non-overwriting cases
-	 *
-	 * note that overwriting only applies to named parameters uniqueness of
-	 * non-named parameters and CLI arguments is ensured by the use of a Set
-	 * internally.
-	 *
-	 * @param string    the argument to be merged into the set of Parameters
-	 * @param overwrite a boolean indicating whether existing parameters should be
-	 *                  overwritten
-	 */
-	public void merge(String string, boolean overwrite) {
-		Matcher matcher = NAMED_PATTERN.matcher(string);
-		if (matcher.find()) {
-			String key = matcher.group("key");
-			String value = matcher.group("value");
-			if (overwrite || !namedParams.containsKey(key)) {
-				namedParams.put(key, value);
-			}
-		} else {
-			unnamedParams.add(string);
-		}
-		rawParams.add(string);
-	}
+	rawParams.add(string);
+    }
 }
