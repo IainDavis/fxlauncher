130af7c621d00fb67a8b7b9f74f51c30d5a8df57
Iain Davis <iain_davis@intuit.com>
2020-12-30 16:21:09 -0800

    change BOOL validation/resolution
    The idea is to let Boolean arguments be provided in a flag style (i.e.,
    --flag) with no value. If a user provides that argument in that format,
    we should default to true.
    
    Change BOOL Validator to accept null, empty, or blank strings
    Change BOOL Resolver to map null, empty, or blank strings to 'true'

--- src/main/java/fxlauncher/config/LauncherOption.java
+++ src/main/java/fxlauncher/config/LauncherOption.java
@@ -6,6 +6,7 @@ import static fxlauncher.model.lifecycle.LifecyclePhase.STARTUP;
 import static java.util.logging.Logger.getLogger;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toCollection;
+import static java.util.stream.Collectors.toSet;
 
 import java.nio.file.Paths;
 import java.util.Arrays;
@@ -27,233 +28,244 @@ import fxlauncher.model.lifecycle.LifecyclePhase;
  * @author idavis1
  */
 public enum LauncherOption {
-	CONFIG_FILE("config-file", true, Defaults.CONFIG_FILE, null, null),
-	OVERRIDES_URL("overrides-url", true, Defaults.NONE, null, Validator.URL),
-	MANIFEST_URL("manifest-url", true, Defaults.NONE, null, Validator.URL),
-	MANIFEST_FILE("manifest-file", true, Defaults.MANIFEST_FILE, null, null),
-	ARTIFACTS_REPO_URL("artifacts-repo-url", true, Defaults.NONE, null, Validator.URL),
-	CACHE_DIR("cache-dir", true, Defaults.CACHE_DIR, Resolver.CACHE_DIR, null),
-	LOG_FILE("log-file", true, Defaults.LOG_FILE, null, null),
-	IGNORE_SSL("ignore-ssl", false, Defaults.BOOL_FALSE, null, Validator.BOOL),
-	OFFLINE("offline", false, Defaults.BOOL_FALSE, null, Validator.BOOL),
-	STOP_ON_UPDATE_ERROR("stop-on-update-error", false, Defaults.BOOL_TRUE, null, Validator.BOOL),
-	ACCEPT_DOWNGRADE("accept-downgrade", false, Defaults.BOOL_FALSE, null, Validator.BOOL),
-	PRELOAD_NATIVE_LIBS("preload-native-libs", true, Defaults.NONE, null, null),
-	HEADLESS("headless", false, Defaults.BOOL_FALSE, null, Validator.BOOL),
-	WHATS_NEW_URL("whats-new-url", true, Defaults.NONE, null, Validator.URL),
-	LINGERING_UPDATE_SCREEN("lingering-update-screen", false, Defaults.BOOL_TRUE, null, Validator.BOOL),;
+  CONFIG_FILE("config-file", true, Defaults.CONFIG_FILE, null, null),
+  OVERRIDES_URL("overrides-url", true, Defaults.NONE, null, Validator.URL),
+  MANIFEST_URL("manifest-url", true, Defaults.NONE, null, Validator.URL),
+  MANIFEST_FILE("manifest-file", true, Defaults.MANIFEST_FILE, null, null),
+  ARTIFACTS_REPO_URL("artifacts-repo-url", true, Defaults.NONE, null, Validator.URL),
+  CACHE_DIR("cache-dir", true, Defaults.CACHE_DIR, Resolver.CACHE_DIR, null),
+  LOG_FILE("log-file", true, Defaults.LOG_FILE, null, null),
+  IGNORE_SSL("ignore-ssl", false, Defaults.BOOL_FALSE, Resolver.BOOL, Validator.BOOL),
+  OFFLINE("offline", false, Defaults.BOOL_FALSE, Resolver.BOOL, Validator.BOOL),
+  STOP_ON_UPDATE_ERROR(
+      "stop-on-update-error", false, Defaults.BOOL_TRUE, Resolver.BOOL, Validator.BOOL),
+  ACCEPT_DOWNGRADE("accept-downgrade", false, Defaults.BOOL_FALSE, Resolver.BOOL, Validator.BOOL),
+  PRELOAD_NATIVE_LIBS("preload-native-libs", true, Defaults.NONE, null, null),
+  HEADLESS("headless", false, Defaults.BOOL_FALSE, Resolver.BOOL, Validator.BOOL),
+  WHATS_NEW_URL("whats-new-url", true, Defaults.NONE, null, Validator.URL),
+  LINGERING_UPDATE_SCREEN(
+      "lingering-update-screen", false, Defaults.BOOL_TRUE, Resolver.BOOL, Validator.BOOL),
+  ;
 
-	private final static Logger log = getLogger(LauncherOption.class.getName());
+  private static final Logger log = getLogger(LauncherOption.class.getName());
 
-	// the string that should be used in a command-line argument or properties file
-	// to set this option
-	private final String label;
-	private final Pattern pattern;
+  // the string that should be used in a command-line argument or properties file
+  // to set this option
+  private final String label;
+  private final Pattern pattern;
 
-	// used to identify when a value has not been explicitly set by a user
-	LifecyclePhase lastSetDuring = STARTUP;
+  // used to identify when a value has not been explicitly set by a user
+  LifecyclePhase lastSetDuring = STARTUP;
 
-	private final String defaultVal;
+  private final String defaultVal;
 
-	private final Resolver resolver;
-	private final Validator validator;
+  private final Resolver resolver;
+  private final Validator validator;
 
-	/**
-	 * Retrieve the set of labels for all {@link LauncherOption} instances
-	 *
-	 * @return the set of labels
-	 */
-	public static Set<String> labels() {
-		return Arrays.asList(values()).stream().map(LauncherOption::getLabel).collect(Collectors.toSet());
-	}
+  /**
+   * Convenience method for returning the values as a {@link Set} (which is iterable)
+   *
+   * @return all {@link LauncherOption} instances as a {@link Set}.
+   */
+  public static Set<LauncherOption> getValueSet() {
+    return Arrays.asList(values()).stream().collect(toSet());
+  }
 
-	/**
-	 * Get a subset of options that satisfy some predicate
-	 *
-	 * @param predicate The predicate that determines which enum instances should be
-	 *                  included in the subset
-	 * @return the set of {@link LauncherOption} objects that satisfy the predicate
-	 *         (return true);
-	 */
-	private static Set<LauncherOption> getSubset(Predicate<LauncherOption> predicate) {
-		return Stream.of(values()).filter(predicate).collect(toCollection(() -> EnumSet.noneOf(LauncherOption.class)));
-	}
+  /**
+   * Retrieve the set of labels for all {@link LauncherOption} instances
+   *
+   * @return the set of labels
+   */
+  public static Set<String> labels() {
+    return Arrays.asList(values())
+        .stream()
+        .map(LauncherOption::getLabel)
+        .collect(Collectors.toSet());
+  }
 
-	/**
-	 * Get all those options that were most recently set from a properties file
-	 *
-	 * @return the set of {@link LauncherOption} objects
-	 */
-	public static Set<LauncherOption> getProps() {
-		return getSubset(LauncherOption::isProp);
-	}
+  /**
+   * Get a subset of options that satisfy some predicate
+   *
+   * @param predicate The predicate that determines which enum instances should be included in the
+   *     subset
+   * @return the set of {@link LauncherOption} objects that satisfy the predicate (return true);
+   */
+  private static Set<LauncherOption> getSubset(Predicate<LauncherOption> predicate) {
+    return Stream.of(values())
+        .filter(predicate)
+        .collect(toCollection(() -> EnumSet.noneOf(LauncherOption.class)));
+  }
 
-	/**
-	 * Get all those options that were most recently set from a command-line
-	 * argument
-	 *
-	 * @return the set of {@link LauncherOption} objects
-	 */
-	public static Set<LauncherOption> getArgs() {
-		return getSubset(LauncherOption::isArg);
-	}
+  /**
+   * Get all those options that were most recently set from a properties file
+   *
+   * @return the set of {@link LauncherOption} objects
+   */
+  public static Set<LauncherOption> getProps() {
+    return getSubset(LauncherOption::isProp);
+  }
 
-	/**
-	 * Get all those options that have not been explicitly set
-	 *
-	 * @return the set of {@link LauncherOption} objects
-	 */
-	public static Set<LauncherOption> getUnset() {
-		return getSubset(LauncherOption::isUnset);
-	}
+  /**
+   * Get all those options that were most recently set from a command-line argument
+   *
+   * @return the set of {@link LauncherOption} objects
+   */
+  public static Set<LauncherOption> getArgs() {
+    return getSubset(LauncherOption::isArg);
+  }
 
-	/**
-	 * Get all those options that have been explicitly set
-	 *
-	 * @return the set of {@link LauncherOption} objects
-	 */
-	public static Set<LauncherOption> getSet() {
-		return getSubset(LauncherOption::isSet);
-	}
+  /**
+   * Get all those options that have not been explicitly set
+   *
+   * @return the set of {@link LauncherOption} objects
+   */
+  public static Set<LauncherOption> getUnset() {
+    return getSubset(LauncherOption::isUnset);
+  }
 
-	/**
-	 * Convenience method allows an action to be performed on all LauncherOption
-	 * intances.
-	 *
-	 * @param action the action to be performed
-	 */
-	public static void forEach(Consumer<LauncherOption> action) {
-		for (LauncherOption opt : values())
-			action.accept(opt);
-	}
+  /**
+   * Get all those options that have been explicitly set
+   *
+   * @return the set of {@link LauncherOption} objects
+   */
+  public static Set<LauncherOption> getSet() {
+    return getSubset(LauncherOption::isSet);
+  }
 
-	private LauncherOption(String label, boolean hasArg, String defaultVal, Resolver resolver, Validator validator) {
-		this.label = label;
-		this.defaultVal = defaultVal;
+  /**
+   * Convenience method allows an action to be performed on all LauncherOption intances.
+   *
+   * @param action the action to be performed
+   */
+  public static void forEach(Consumer<LauncherOption> action) {
+    for (LauncherOption opt : values()) action.accept(opt);
+  }
 
-		String patternString = Stream.of("--", label, hasArg ? "=(.+)" : "").collect(joining());
-		pattern = Pattern.compile(patternString);
+  private LauncherOption(
+      String label, boolean hasArg, String defaultVal, Resolver resolver, Validator validator) {
+    this.label = label;
+    this.defaultVal = defaultVal;
 
-		this.resolver = resolver == null ? Resolver.DEFAULT : resolver;
-		this.validator = validator == null ? Validator.DEFAULT : validator;
-	}
+    String patternString = Stream.of("--", label, hasArg ? "=(.+)" : "").collect(joining());
+    pattern = Pattern.compile(patternString);
 
-	/**
-	 * get the label from this {@link LauncherOption} instance
-	 *
-	 * @return the label for a particular {@link LauncherOption}
-	 */
-	public String getLabel() {
-		return this.label;
-	}
+    this.resolver = resolver == null ? Resolver.DEFAULT : resolver;
+    this.validator = validator == null ? Validator.DEFAULT : validator;
+  }
 
-	/**
-	 * get the default value from this {@link LauncherOption}
-	 *
-	 * @return the default value for a particular {@link LauncherOption}
-	 */
-	public String getDefault() {
-		return this.defaultVal;
-	}
+  /**
+   * get the label from this {@link LauncherOption} instance
+   *
+   * @return the label for a particular {@link LauncherOption}
+   */
+  public String getLabel() {
+    return this.label;
+  }
 
-	/**
-	 * check whether this @{link LauncherOption} was last set from a properties file
-	 *
-	 * @return {@code true} if this {@link LauncherOption} was last set from a
-	 *         properties file, {@code false} otherwise
-	 */
-	public boolean isProp() {
-		return lastSetDuring == LOAD_EMBEDDED_CONFIG;
-	}
+  /**
+   * get the default value from this {@link LauncherOption}
+   *
+   * @return the default value for a particular {@link LauncherOption}
+   */
+  public String getDefault() {
+    return this.defaultVal;
+  }
 
-	/**
-	 * check whether this {@link LauncherOption} was last set from a command-line
-	 * argument
-	 *
-	 * @return {@code true} if this {@link LauncherOption} was last set from a
-	 *         command-line argument, {@code false} otherwise
-	 */
-	public boolean isArg() {
-		return lastSetDuring == PARSE_CLI_ARGS;
-	}
+  /**
+   * check whether this @{link LauncherOption} was last set from a properties file
+   *
+   * @return {@code true} if this {@link LauncherOption} was last set from a properties file, {@code
+   *     false} otherwise
+   */
+  public boolean isProp() {
+    return lastSetDuring == LOAD_EMBEDDED_CONFIG;
+  }
 
-	/**
-	 * check whether this {@link LauncherOption} has been explicitly set
-	 *
-	 * @return {@code true} if this {@link LauncherOption} has ever been explicitly
-	 *         set, {@code false} otherwise
-	 */
-	public boolean isSet() {
-		return lastSetDuring != STARTUP;
-	}
+  /**
+   * check whether this {@link LauncherOption} was last set from a command-line argument
+   *
+   * @return {@code true} if this {@link LauncherOption} was last set from a command-line argument,
+   *     {@code false} otherwise
+   */
+  public boolean isArg() {
+    return lastSetDuring == PARSE_CLI_ARGS;
+  }
 
-	/**
-	 * check whether this {@link LauncherOption} has not been explicitly set
-	 *
-	 * @return {@code false} if this {@link LauncherOption} has ever been explicitly
-	 *         set, {@code true} otherwise
-	 */
-	public boolean isUnset() {
-		return !this.isSet();
-	}
+  /**
+   * check whether this {@link LauncherOption} has been explicitly set
+   *
+   * @return {@code true} if this {@link LauncherOption} has ever been explicitly set, {@code false}
+   *     otherwise
+   */
+  public boolean isSet() {
+    return lastSetDuring != STARTUP;
+  }
 
-	/**
-	 * get a Matcher object that can be used to match a String to a
-	 * {@link LauncherOption} used to resolve properties and command-line arguments
-	 *
-	 * @param arg
-	 * @return a {@link Matcher} created from the {@link LauncherOption}'s label and
-	 *         the string to be matched.
-	 */
-	public Matcher getMatcher(String arg) {
-		return pattern.matcher(arg);
-	}
+  /**
+   * check whether this {@link LauncherOption} has not been explicitly set
+   *
+   * @return {@code false} if this {@link LauncherOption} has ever been explicitly set, {@code true}
+   *     otherwise
+   */
+  public boolean isUnset() {
+    return !this.isSet();
+  }
 
-	/**
-	 * get the {@link Resolver} registered to a {@link LauncherOption}
-	 *
-	 * @return the {@link Resolver}
-	 */
-	Resolver getResolver() {
-		return resolver;
-	}
+  /**
+   * get a Matcher object that can be used to match a String to a {@link LauncherOption} used to
+   * resolve properties and command-line arguments
+   *
+   * @param arg an argument that might match one of the {@link LauncherOption} instances.
+   * @return a {@link Matcher} created from the {@link LauncherOption}'s label and the string to be
+   *     matched.
+   */
+  public Matcher getMatcher(String arg) {
+    return pattern.matcher(arg);
+  }
 
-	/**
-	 * get the {@link Validator} registered to a {@link LauncherOption}
-	 *
-	 * @return
-	 */
-	Validator getValidator() {
-		return validator;
-	}
+  /**
+   * get the {@link Resolver} registered to a {@link LauncherOption}
+   *
+   * @return the {@link Resolver}
+   */
+  Resolver getResolver() {
+    return resolver;
+  }
 
-	/**
-	 * make a note of the most recent time this {@link LauncherOption} was set
-	 *
-	 * @param phase
-	 */
-	void recordOptionSet(LifecyclePhase phase) {
-		log.finer(String.format("Option '%s' set during phase '%s'", this, phase));
-		this.lastSetDuring = phase;
-	}
+  /**
+   * get the {@link Validator} registered to a {@link LauncherOption}
+   *
+   * @return the validator for this {@link LauncherOption} instance
+   */
+  Validator getValidator() {
+    return validator;
+  }
 
-	/**
-	 * The set of default values for FxLauncher {@link LauncherOption} objects
-	 *
-	 * @implNote these are included in an inner class rather than as standard
-	 *           'private static final String' constants because Java does not
-	 *           provide access to static objects during execution of the enum
-	 *           constructors.
-	 *
-	 * @author idavis1
-	 */
-	private static class Defaults {
-		private static final String CONFIG_FILE = "launcher.properties";
-		private static final String LOG_FILE = System.getProperty("java.io.tmpdir") + "fxlauncher.log";
-		private static final String MANIFEST_FILE = "app.xml";
-		private static final String CACHE_DIR = Paths.get(".").toString();
-		private static final String BOOL_FALSE = Boolean.FALSE.toString();
-		private static final String BOOL_TRUE = Boolean.TRUE.toString();
-		private static final String NONE = null;
-	}
+  /**
+   * make a note of the most recent time this {@link LauncherOption} was set
+   *
+   * @param phase the {@link LifecyclePhase} during which this option was last set
+   */
+  void recordOptionSet(LifecyclePhase phase) {
+    log.finer(String.format("Option '%s' set during phase '%s'", this, phase));
+    this.lastSetDuring = phase;
+  }
+
+  /**
+   * The set of default values for FxLauncher {@link LauncherOption} objects
+   *
+   * @implNote these are included in an inner class rather than as standard 'private static final
+   *     String' constants because Java does not provide access to static objects during execution
+   *     of the enum constructors.
+   * @author idavis1
+   */
+  private static class Defaults {
+    private static final String CONFIG_FILE = "launcher.properties";
+    private static final String LOG_FILE = System.getProperty("java.io.tmpdir") + "fxlauncher.log";
+    private static final String MANIFEST_FILE = "app.xml";
+    private static final String CACHE_DIR = Paths.get(".").toString();
+    private static final String BOOL_FALSE = Boolean.FALSE.toString();
+    private static final String BOOL_TRUE = Boolean.TRUE.toString();
+    private static final String NONE = null;
+  }
 }
